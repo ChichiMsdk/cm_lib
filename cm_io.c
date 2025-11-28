3a7f7b31f312ad69d6380d476109d8e7d17fd378
ChichiMsdk <111235918+ChichiMsdk@users.noreply.github.com>
2025-11-28 18:46:01 +0100

    Bug fixes

--- cm_io.c
+++ cm_io.c
@@ -39,11 +39,11 @@ typedef struct cmFile
 force_inline CM_CODE
 unmap_view_of_file(void *base)
 {
-  BOOL    value       = TRUE;
-  CM_CODE error_value = CM_OK;
+  BOOL    value;
+  CM_CODE error_value;
 
+  error_value = CM_OK;
   if (!base) return error_value;
-
   value = UnmapViewOfFile(base);
   if (!value) error_value = CM_INVALID_DATA;
   return error_value;
@@ -52,27 +52,30 @@ unmap_view_of_file(void *base)
 force_inline CM_CODE
 file_mapping_create(cmFile* file, u32 fl_protect, u32 mapping_max_size)
 {
-  u32     low         = mapping_max_size;
-  u32     high        = 0;
-  CM_CODE error_value = CM_OK;
-  low = ((u32) file->file_size > low) ? low : (u32) file->file_size;
+  u32     low, high, file_size;
+  CM_CODE error_value;
+
+  file_size   = (u32) file->file_size;
+  high        = 0;
+  low         = (file_size > mapping_max_size) ? mapping_max_size : file_size;
+  error_value = CM_OK;
   file->h_map = CreateFileMapping(file->h_file, NULL, fl_protect, high, low, NULL);
-  if (!file->h_map)
-  {
-    error_value = CM_FILE_MAP_FAIL;
-  }
+  if (!file->h_map) error_value = CM_FILE_MAP_FAIL;
   return error_value;
 }
 
 force_inline CM_CODE
 file_open(char* path, u32 access, u32 share, cmFile* file)
 {
-  u32     c           = OPEN_EXISTING;
-  u32     attr        = FILE_ATTRIBUTE_NORMAL;
-  CM_CODE error_value = CM_OK;
+  u32     creation, attr;
+  CM_CODE error_value;
 
-  file->h_file = CreateFile(path, access, share, NULL, c, attr, NULL);
-  if (!file->h_file || file->h_file == INVALID_HANDLE_VALUE)
+  creation    = OPEN_EXISTING;
+  attr        = FILE_ATTRIBUTE_NORMAL;
+  error_value = CM_OK;
+  memset(file, 0, sizeof(cmFile));
+  file->h_file = CreateFile(path, access, share, NULL, creation, attr, NULL);
+  if (file->h_file == INVALID_HANDLE_VALUE)
   {
     error_value = CM_FILE_OPEN_FAIL;
 		report_error_box("CreateFile");
@@ -90,22 +93,16 @@ file_open(char* path, u32 access, u32 share, cmFile* file)
   return error_value;
 }
 
-static inline CM_CODE
-file_write(char* buffer, u32 size, cmFile* f)
-{
-  CM_CODE error_value = CM_OK; 
-  i64 written = write_file(f->h_file, buffer, size);
-  if (written == -1) error_value = CM_FILE_WRITE_FAIL;
-  return error_value;
-}
-
 force_inline CM_CODE
 file_create(char* path, u32 access, u32 share, u32 c, cmFile* f)
 {
-  u32     attr        = FILE_ATTRIBUTE_NORMAL;
-  CM_CODE error_value = CM_OK;
+  u32     attr;
+  CM_CODE error_value;
+
+  attr        = FILE_ATTRIBUTE_NORMAL;
+  error_value = CM_OK;
   f->h_file = CreateFile(path, access, share, NULL, c, attr, NULL);
-  if (!f->h_file || f->h_file == INVALID_HANDLE_VALUE)
+  if (f->h_file == INVALID_HANDLE_VALUE)
   {
     error_value = CM_FILE_OPEN_FAIL;
   }
@@ -122,28 +119,36 @@ file_create(char* path, u32 access, u32 share, u32 c, cmFile* f)
   return error_value;
 }
 
+static inline CM_CODE
+file_write(char* buffer, u32 size, cmFile* f)
+{
+  i64     written;
+  CM_CODE error_value; 
+
+  written     = write_file(f->h_file, buffer, size);
+  error_value = CM_OK;
+  if (written == -1) error_value = CM_FILE_WRITE_FAIL;
+  return error_value;
+}
+
 force_inline CM_CODE 
 handle_close(HANDLE h)
 {
-  CM_CODE error_value = CM_OK;
-  if (h != INVALID_HANDLE_VALUE && h != NULL)
-  {
-    if (CloseHandle(h) == 0)
-    {
-      error_value = CM_API_FAIL;
-    }
-  }
+  CM_CODE error_value;
+
+  error_value = CM_OK;
+  if (h == INVALID_HANDLE_VALUE || !h) { return error_value; }
+  if ( !CloseHandle(h) )               { error_value = CM_API_FAIL; }
   return error_value;
 }
 
 static CM_CODE
 file_close(cmFile* file)
 {
-  CM_CODE error_value = CM_OK;
-  if (!file)
-  {
-    error_value = CM_INVALID_DATA;
-  }
+  CM_CODE error_value;
+
+  error_value = CM_OK;
+  if (!file) error_value = CM_INVALID_DATA;
   if (error_value == CM_OK)
   {
     /* NOTE: Might as well check those, maybe log them somewhere ? */
@@ -160,36 +165,26 @@ file_close(cmFile* file)
 static CM_CODE
 file_dump(char* path, char* buffer, u32 size, cmFile* f)
 {
-  CM_CODE error_value = CM_OK;
-  error_value = file_create(path, GENERIC_READ | GENERIC_WRITE, FILE_SHARE_READ, CREATE_ALWAYS, f);
+  CM_CODE error_value;
 
+  error_value = file_create(path, GENERIC_READ | GENERIC_WRITE, FILE_SHARE_READ, CREATE_ALWAYS, f);
   if (error_value == CM_OK) error_value = file_write(buffer, size, f);
   if (error_value == CM_OK) error_value = file_close(f);
   return error_value;
 }
 
 static CM_CODE
-file_exist_open_map_sized(
-    char*                 path,
-    cmFile*               file,
-    u32                   max_size,
-    u32                   access,
-    u32                   fl_protec,
-    u32                   desired)
+file_exist_open_map_sized(char* path, cmFile *file, u32 max_size, u32 access, u32 fl_protec, u32 desired)
 {
-  CM_CODE     error_value = CM_OK;
-  SYSTEM_INFO sys         = {0};
-  DWORD       gran        = 0;
+  DWORD       gran;
+  CM_CODE     error_value;
+  SYSTEM_INFO sys;
 
   GetSystemInfo(&sys);
   gran = sys.dwAllocationGranularity;
   error_value = file_open(path, access, FILE_SHARE_READ, file);
-
-  if (error_value == CM_OK)
-  {
-    /* NOTE: We probably want to log this */
-    error_value = file_mapping_create(file, fl_protec, max_size);
-  }
+  /* NOTE: We probably want to log this */
+  if (error_value == CM_OK) error_value = file_mapping_create(file, fl_protec, max_size);
   if (error_value == CM_OK)
   {
     /* NOTE: we map the entire view of the file in memory */
@@ -203,46 +198,59 @@ file_exist_open_map_sized(
 force_inline CM_CODE
 file_exist_open_map_rw(char* path, cmFile* file)
 {
-  u32 max_size = MB(50);
-  return file_exist_open_map_sized(
-      path,
-      file,
-      max_size,
-      GENERIC_READ | GENERIC_WRITE,
-      PAGE_READWRITE,
-      FILE_MAP_READ | FILE_MAP_WRITE);
+  u32 max_size, generic, map;
+
+  map      = FILE_MAP_READ | FILE_MAP_WRITE;
+  generic  = GENERIC_READ | GENERIC_WRITE;
+  max_size = MB(50);
+  return file_exist_open_map_sized(path, file, max_size, generic, PAGE_READWRITE, map);
 }
 
 force_inline CM_CODE
 file_exist_open_map_ro(char* path, cmFile* file)
 {
-  u32 max_size = MB(50);
-  return file_exist_open_map_sized(
-      path,
-      file,
-      max_size,
-      GENERIC_READ,
-      PAGE_READONLY,
-      FILE_MAP_READ);
+  u32 max_size;
+
+  max_size = MB(50);
+  return file_exist_open_map_sized( path, file, max_size, GENERIC_READ, PAGE_READONLY, FILE_MAP_READ);
 }
 
-int64_t
+force_inline void 
+console_alloc(void)
+{
+  BOOL value;
+
+  value = AllocConsole();
+}
+
+force_inline void 
+console_free(void)
+{
+  BOOL value;
+
+  value = FreeConsole();
+}
+
+static inline i64
 console_write(HANDLE h, void* str, u32 len)
 {
-  DWORD bytes_written = 0;
-  BOOL value = WriteConsole(h,str, len, &bytes_written, NULL);
+  u32   bytes_written;
+  BOOL  value;
+
+  value = WriteConsole(h,str, len, &bytes_written, NULL);
   if (value == 0) return -1;
   return bytes_written;
 }
 
-bool
+static inline bool
 console_pause(void)
 {
-  DWORD         eventsRead  = 0;
-  DWORD         written     = 0;
-  char*         msg         = "Press any key to continue . . .\r\n";
-  INPUT_RECORD  inputRecord = {0};
+  char          *msg
+  BOOL          key_down;
+  DWORD         eventsRead;
+  INPUT_RECORD  inputRecord;
 
+  msg = "Press any key to continue . . .\r\n";
   if (console_write(STDOUT(), msg, (u32) strlen(msg)) == -1)
   {
     report_error("console_write");
@@ -255,7 +263,7 @@ console_pause(void)
       report_error("ReadConsole");
       return false;
     }
-    BOOL key_down = inputRecord.Event.KeyEvent.bKeyDown;
+    key_down = inputRecord.Event.KeyEvent.bKeyDown;
     if (inputRecord.EventType == KEY_EVENT && key_down)
       break;
   }
